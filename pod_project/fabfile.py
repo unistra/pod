58b13179f4fd824d3b12af67468db134ce68111c
Morgan <morgan.bohn@unistra.fr>
2015-12-08 15:55:59 +0100

    pydiploy compatibility: nginx conf and chaussette for vagrant dev

--- pod_project/fabfile.py
+++ pod_project/fabfile.py
@@ -64,7 +64,7 @@ env.chaussette_backend = 'waitress' # name of chaussette backend to use. You nee
 env.media_folder = '/media' # path of media folder
 env.remote_media_folder = '/srv/media/pod' # root of media files
 
-# env.nginx_location_extra_directives = ['proxy_read_timeout 120'] # add directive(s) to nginx config file in location part
+env.nginx_location_extra_directives = ['client_max_body_size 3G'] # add directive(s) to nginx config file in location part
 # env.nginx_start_confirmation = True # if True when nginx is not started
 # needs confirmation to start it.
 
