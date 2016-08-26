734a95468e585cb0202f63997d80dd553c61ad01
Morgan <morgan.bohn@unistra.fr>
2016-08-26 15:25:07 +0200

    migration avcast : import all buildings and amphis

---
+++ pod_project/migration_avcast/management/commands/import_buildings_and_amphis.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+"""
+Import all avcast's buildings and amphis in pod
+"""
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+import psycopg2
+import psycopg2.extras
+from pods.models import Recorder, Building
+
+
+class Command(BaseCommand):
+    help = "Import all avcast's buildings and amphis in pod"
+
+    def handle(self, *args, **options):
+        self.stdout.write("Import all buildings and amphis ...")
+        conn = None
+        try:
+            conn = psycopg2.connect(settings.AVCAST_DB_URI)
+            with conn:
+                with conn.cursor(
+                    cursor_factory=psycopg2.extras.RealDictCursor
+                ) as curs:
+                    curs.execute(
+                        ('SELECT b.name as bname,a.name as aname,a.ipaddress,'
+                         'a.status,a.gmapurl,a.restrictionuds '
+                         'FROM amphi a '
+                         'INNER JOIN building b '
+                         'ON a.buildingid = b.buildingid')
+                    )
+                    for row in curs.fetchall():
+                        # create or modify building
+                        building, created = Building.objects.get_or_create(
+                            name=row['bname']
+                        )
+
+                        # create or modify amphi
+                        recorder, created = Recorder.objects.get_or_create(
+                            name=row['aname'],
+                            building=building,
+                            adress_ip=row['ipaddress']
+                        )
+                        recorder.status = row['status'],
+                        recorder.gmapurl = row['gmapurl']
+                        recorder.is_restricted = row['restrictionuds']
+                        recorder.save()
+
+        except psycopg2.DatabaseError as e:
+            raise e
+            raise CommandError("Cannot access to the database ")
+        finally:
+            if conn:
+                conn.close()
+                self.stdout.write("Done !")
