d5beb10df23a7d79a48b9d1e887e0d9b6dcd6de0
Morgan <morgan.bohn@unistra.fr>
2016-08-29 17:00:26 +0200

    import : ca videoslide

--- pod_project/migration_avcast/management/commands/import_courses.py
+++ pod_project/migration_avcast/management/commands/import_courses.py
@@ -8,9 +8,71 @@ from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 import psycopg2
 import psycopg2.extras
-from pods.models import Pod, Type, ContributorPods, Discipline
+from pods.models import Pod, Type, ContributorPods, Discipline, EncodingPods
+from core.models import EncodingType, get_storage_path
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+
+
+
+# /** media type available **/
+# public static final int typeFlash = 1;
+# public static final int typeMp3 = 2;
+# public static final int typeOgg = 4;
+# public static final int typePdf = 8;
+# public static final int typeZip = 16;
+# public static final int typeVideoslide = 32;
+# public static final int typeAdddoc = 64;
+# public static final int typeHq = 128;
+# public static final int typeVideoslideIpod = 256;
+# public static final int typeSubtitles = 512;
+# public static final int typeAddVideo = 1024;
+# public static final int typeWebm = 2048;
+#
+#
+# /**
+#  * Check the media availability
+#  * @param type media type(mp3,ogg,...)
+#  * @return true if the media is available
+#  */
+# public boolean isAvailable(String type) {
+#         if(type == null || type.equals("")) return false;
+#         else if(type.equals("flash")) return ((typeFlash & mediatype) > 0);
+#         else if(type.equals("mp3")) return ((typeMp3 & mediatype) > 0);
+#         else if(type.equals("ogg")) return ((typeOgg & mediatype) > 0);
+#         else if(type.equals("pdf")) return ((typePdf & mediatype) > 0);
+#         else if(type.equals("zip")) return ((typeZip & mediatype) > 0);
+#         else if(type.equals("videoslide")) return ((typeVideoslide & mediatype) > 0);
+#         else if(type.equals("adddoc")) return ((typeAdddoc & mediatype) > 0);
+#         else if(type.equals("hq")) return ((typeHq & mediatype) > 0);
+#         else if(type.equals("video")) return ((typeHq & mediatype) > 0);
+#         else if(type.equals("videoslideipod")) return ((typeVideoslideIpod & mediatype) > 0);
+#         else if(type.equals("subtitles")) return ((typeSubtitles & mediatype) > 0);
+#         else if(type.equals("addvideo")) return ((typeAddVideo & mediatype) > 0);
+#         else if(type.equals("webm")) return ((typeWebm & mediatype) > 0);
+#         else if(type.equals("html5")) {
+#                 if(this.type.equals("audio"))
+#                         return  ((typeMp3 & mediatype) > 0);
+#                 else
+#                         return ((typeHq & mediatype) > 0) || ((typeWebm & mediatype) > 0);
+#         }
+#         else return false;
+# }
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class Command(BaseCommand):
@@ -160,17 +222,76 @@ class Command(BaseCommand):
                             pod.allow_downloading = row['download']
                             pod.is_restricted = row['restrictionuds']
                             pod.date_evt = row['recorddate']
+                            pod.encoding_status = 'AVCAST'
                             # formation
                             pod = self.pod_add_discipline_and_cursus(conn, pod, row)
                             # Add author and tags
                             pod = self.pod_add_author(pod, row)
                             pod = self.pod_add_tags(conn, pod, row)
 
-                            # TODO adddocname => il faut créer un document avec chemin du cours + adddocname dans path
 
-                            # TODO créer encodage (mediatype ?)
+
+                            ###################### TODO créer encodage (mediatype ?)
+
+                            mediatype = row['mediatype']
+
+                            is_videoslide_present = 32 & mediatype > 0
+                            is_mp3_present = 2 & mediatype > 0
+                            is_videomp4_present = 128 & mediatype > 0
+                            is_addvideo_present = 1024 & mediatype > 0
+
+                            # MUA or CA
+                            if row['type'] == "audio":
+                                # Use the videoslide if he is present (CA)
+                                if is_videoslide_present:
+                                    file_path = get_storage_path(pod,"%s/%s_videoslide.mp4" % (pod.id, pod.id))
+                                    ep, ep_created = EncodingPods.objects.get_or_create(
+                                        video=pod,
+                                        encodingType=EncodingType.objects.get(name="720"),
+                                        encodingFile=file_path,
+                                        encodingFormat="video/mp4"
+                                    )
+                                    pod.video = file_path
+                                # TODO else, use mp3 (MUA)
+                                elif is_mp3_present:
+                                    pass
+                                else:
+                                    raise CommandError("No media for %s" % pod.id)
+                            # MUV or CV or CSC
+                            else:
+                                # TODO Use the videoslide if he is present (CV)
+                                if is_videoslide_present:
+                                    pass
+                                # TODO priority to additional video
+                                elif is_addvideo_present:
+                                    pass
+                                # TODO else, use mp4 (MUV or CSC)
+                                elif is_videomp4_present:
+                                    pass
+                                elif is_mp3_present:
+                                    pass
+                                else:
+                                    raise CommandError("No media for %s" % pod.id)
+
+
+                            # encodingpods, ep_created = EncodingPods.objects.get_or_create(
+                            #     video=pod,
+                            #     encodingType=None,
+                            #     encodingFile=None,
+                            #     encodingFormat=None
+                            #
+                            # )
+
+
+
+                            ##################################
+
+
+                            # TODO adddocname => il faut créer un document avec chemin du cours + adddocname dans path
+                            is_addoc_present = 64 & mediatype > 0
 
                             # TODO piste sous titres ? Avcast utilise le format TTML, pod le format WEBVTT
+                            is_subtitles_present = 512 & mediatype > 0
 
                             # Save all modification
                             pod.save()
