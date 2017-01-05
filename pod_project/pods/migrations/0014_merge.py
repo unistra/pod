3c56ad03a2f6aff95b6e03f4bacd45eb3a4280ed
Morgan <morgan.bohn@unistra.fr>
2017-01-05 14:37:40 +0100

    maj settings

---
+++ pod_project/pods/migrations/0014_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pods', '0013_pod_is_360'),
+        ('pods', '0013_auto_20160927_1414'),
+    ]
+
+    operations = [
+    ]
