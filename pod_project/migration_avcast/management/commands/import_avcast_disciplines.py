f6387350b558b097c31db3f3ed5b77093a3f83ab
Morgan <morgan.bohn@unistra.fr>
2017-04-05 15:58:24 +0200

    use channel and theme for avcast discipline instead of pod discipline

--- pod_project/migration_avcast/management/commands/import_avcast_disciplines.py
+++ pod_project/migration_avcast/management/commands/import_avcast_disciplines.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 
 """
-Import all avcast's disciplines in pod
+Import all avcast's disciplines in pod's channels and themes
 """
 from __future__ import unicode_literals
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 import psycopg2
 import psycopg2.extras
-from pods.models import Discipline
+from pods.models import Discipline, Channel, Theme
 from django.template.defaultfilters import slugify
 
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 
 
 class Command(BaseCommand):
-    help = "Import all avcast's disciplines in pod"
+    help = "Import all avcast's disciplines in pod's channels and themes"
 
     def handle(self, *args, **options):
         # Check settings
@@ -34,18 +34,41 @@ class Command(BaseCommand):
                          'FROM discipline')
                     )
                     for row in curs.fetchall():
-                        # create or modify disciplines
-                        discipline, created = Discipline.objects.get_or_create(
-                            slug=slugify(row['namecomp'])
+                        # # create or modify disciplines
+                        # discipline, created = Discipline.objects.get_or_create(
+                        #     slug=slugify(row['namecomp'])
+                        # )
+                        # discipline.title = row['namecomp']
+                        # discipline.description = "codecomp=%s;codedom=%s;namedom=%s" % (
+                        #     row['codecomp'] if row['codecomp'] else "",
+                        #     row['codedom'] if row['codedom'] else "",
+                        #     row['namedom'] if row['namedom'] else ""
+                        # )
+                        # discipline.save()
+                        # self.stdout.write(self.style.SQL_FIELD('Discipline "%s" saved !' % discipline.title))
+
+                        # create or modify channels
+                        channel, created = Channel.objects.get_or_create(
+                            slug=slugify(row['namedom'])
                         )
-                        discipline.title = row['namecomp']
-                        discipline.description = "codecomp=%s;codedom=%s;namedom=%s" % (
-                            row['codecomp'] if row['codecomp'] else "",
+                        channel.title = row['namedom']
+                        channel.visible = True
+                        channel.description = "codedom=%s" % (
                             row['codedom'] if row['codedom'] else "",
-                            row['namedom'] if row['namedom'] else ""
                         )
-                        discipline.save()
-                        self.stdout.write(self.style.SQL_FIELD('Discipline "%s" saved !' % discipline.title))
+                        channel.save()
+                        self.stdout.write(self.style.SQL_FIELD('Channel "%s" saved !' % channel.title))
+                        # create or modify theme
+                        theme, created = Theme.objects.get_or_create(
+                            slug=slugify(row['namecomp']),
+                            channel=channel
+                        )
+                        theme.title = row['namecomp']
+                        theme.description = "codecomp=%s" % (
+                            row['codecomp'] if row['codecomp'] else "",
+                        )
+                        theme.save()
+                        self.stdout.write(self.style.SQL_FIELD('Theme "%s" saved !' % channel.title))
         except psycopg2.DatabaseError as e:
             raise e
             raise CommandError("Cannot access to the database ")
